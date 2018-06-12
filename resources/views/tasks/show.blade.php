d89c2ae850d08b29656b9adb0b84840d2c690e50
yui.ono <yui.a.ono@rakuten.com>
2018-06-12 06:55:30 +0000

    tasklist

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -4,7 +4,7 @@
 
  <h1>id = {{ $task->id }} のタスク詳細ページ</h1>
  
- <p>ステイタス: {{ $task->title }}</p>
+ <p>ステイタス: {{ $task->status }}</p>
  <p>タスク: {{ $task->content }}</p>
 
   {!! link_to_route('tasks.edit', 'このタスク編集', ['id' => $task->id]) !!}
