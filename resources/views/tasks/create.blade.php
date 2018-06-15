e484f038a4b9862be2e775ebeabad6da59cbfd0f
yui.ono <yui.a.ono@rakuten.com>
2018-06-15 07:44:28 +0000

    bootstrap design

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -8,7 +8,7 @@
             {!! Form::model($task, ['route' => 'tasks.store']) !!}
             
     <div class="row">
-        <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-8">
+        <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
             {!! Form::model($task, ['route' => 'tasks.store']) !!}
             
                 <div class="form-group">
